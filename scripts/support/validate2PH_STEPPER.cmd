09dbb2fff21d1404c029cd651c74752bf8b179a6
Anders Sandström <anders.sandstroem@psi.ch>
2023-12-18 12:13:06 +0100

    Auto reduce current to max drv current

--- scripts/support/validate2PH_STEPPER.cmd
+++ scripts/support/validate2PH_STEPPER.cmd
@@ -7,26 +7,61 @@
 
 #-===== Validate HW_DESC matches component ===
 ecmcEpicsEnvSetCalcTernary(DIE,"'2PH_STEPPER' in '${SLAVE_TYPE}'","#-", "")
-${DIE}ecmcExit Error: Component and slave do not matching
+${DIE}ecmcExit Error: Component and slave do not match
+
 
 #-===== Validate currents ===
-#- Run current
-epicsEnvSet(I_MAX_MA, ${I_MAX_MA=${MOT_I_MAX_MA}})
+#- =========== Max current ============
+
+#- Reduce allowed motor current if drive cannot support the current
+ecmcEpicsEnvSetCalcTernary(MOT_I_MAX_MA,"${MOT_I_MAX_MA}>${DRV_I_MAX_MA}",${DRV_I_MAX_MA},${MOT_I_MAX_MA})
+
+epicsEnvSet(I_MAX_MA_VALID, ${I_MAX_MA=${MOT_I_MAX_MA}})
+
+#- Die if to high current
+ecmcEpicsEnvSetCalcTernary(DIE,"${I_MAX_MA_VALID}>${MOT_I_MAX_MA}","", "#-")
+${DIE}ecmcExit Error: Current setting too high for motor
+
+#- Die if to high current
+ecmcEpicsEnvSetCalcTernary(DIE,"${I_MAX_MA_VALID}>${DRV_I_MAX_MA}","", "#-")
+${DIE}ecmcExit Error: Current setting too high for drive
 
 #- Ensure I_MAX_MA <= MOT_I_MAX_MA
-ecmcEpicsEnvSetCalcTernary(I_MAX_MA,"${I_MAX_MA}>${MOT_I_MAX_MA}",${MOT_I_MAX_MA},${I_MAX_MA})
+ecmcEpicsEnvSetCalcTernary(I_MAX_MA_VALID,"${I_MAX_MA_VALID}>${MOT_I_MAX_MA}",${MOT_I_MAX_MA},${I_MAX_MA})
 
-#- Ensure I_MAX_MA <= DRV_I_MAX_MA
-ecmcEpicsEnvSetCalcTernary(I_MAX_MA_VALID,"${I_MAX_MA}>${DRV_I_MAX_MA}",${DRV_I_MAX_MA},${I_MAX_MA})
 
-#- Stdby current
+#- =========== Stdby current ============
 epicsEnvSet(I_STDBY_MA_VALID, ${I_STDBY_MA=${MOT_I_STDBY_MA}})
 
-#- Ensure I_STDBY_MA <= I_MAX_MA
-ecmcEpicsEnvSetCalcTernary(I_STDBY_MA_VALID,"${I_STDBY_MA}>${I_MAX_MA}",${I_MAX_MA},${I_STDBY_MA})
+#- Die if to high current
+ecmcEpicsEnvSetCalcTernary(DIE,"${I_STDBY_MA_VALID}>${MOT_I_MAX_MA}","", "#-")
+${DIE}ecmcExit Error: Standby Current setting too high for motor
 
-#-===== Validate voltage ===
+#- Die if to high current
+ecmcEpicsEnvSetCalcTernary(DIE,"${I_STDBY_MA_VALID}>${DRV_I_MAX_MA}","", "#-")
+${DIE}ecmcExit Error: Standby Current setting too high for drive
+
+#- Die if I_STDBY_MA_VALID > I_MAX_MA_VALID
+ecmcEpicsEnvSetCalcTernary(DIE,"${I_STDBY_MA_VALID}>${I_MAX_MA_VALID}","", "#-")
+${DIE}ecmcExit Error: Standby Current setting higher than run current
+
+#- Ensure I_STDBY_MA <= I_MAX_MA
+ecmcEpicsEnvSetCalcTernary(I_STDBY_MA_VALID,"${I_STDBY_MA_VALID}>${I_MAX_MA}",${I_MAX_MA},${I_STDBY_MA})
+
+
+#- =========== Voltage ============
+
+#- Reduce the allowed motor voltage if drive cannot support the voltage
+ecmcEpicsEnvSetCalcTernary(MOT_U_MAX_MV,"${MOT_U_MAX_MV}>${DRV_U_MAX_MV}",${DRV_U_MAX_MV},${MOT_U_MAX_MV})
+
 epicsEnvSet(U_NOM_MV, ${U_NOM_MV=${MOT_U_MAX_MV}})
+
+#- Die if to high voltage
+ecmcEpicsEnvSetCalcTernary(DIE,"${U_NOM_MV}>${MOT_U_MAX_MV}","", "#-")
+${DIE}ecmcExit Error: Nominal voltage too high for motor
+
+#- ecmcEpicsEnvSetCalcTernary(DIE,"${U_NOM_MV}>${DRV_U_MAX_MV}","", "#-")
+#- ${DIE}ecmcExit Error: Nominal voltage too high for drive
 
 #- Ensure U_NOM_MV <= DRV_U_MAX_MV
 ecmcEpicsEnvSetCalcTernary(U_NOM_MV_VALID,"${U_NOM_MV}>${DRV_U_MAX_MV}",${DRV_U_MAX_MV},${U_NOM_MV})
--- scripts/support/validateEL72XX_XXX_SERVO.cmd
+++ scripts/support/validateEL72XX_XXX_SERVO.cmd
@@ -6,17 +6,33 @@
 #-d   \file
 
 #-===== Validate currents ===
+
+#- Reduce allowed motor current if drive cannot support the current
+ecmcEpicsEnvSetCalcTernary(MOT_I_MAX_MA,"${MOT_I_MAX_MA}>${DRV_I_MAX_MA}",${DRV_I_MAX_MA},${MOT_I_MAX_MA})
+
 #- Run current
-epicsEnvSet(I_MAX_MA, ${I_MAX_MA=${MOT_I_MAX_MA}})
+epicsEnvSet(I_MAX_MA_VALID, ${I_MAX_MA=${MOT_I_MAX_MA}})
+
+#- Die if to high current
+ecmcEpicsEnvSetCalcTernary(DIE,"${I_MAX_MA_VALID}>${MOT_I_MAX_MA}","", "#-")
+${DIE}ecmcExit Error: Current setting too high for motor
+
+#- Die if to high current
+ecmcEpicsEnvSetCalcTernary(DIE,"${I_MAX_MA_VALID}>${DRV_I_MAX_MA}","", "#-")
+${DIE}ecmcExit Error: Current setting too high for drive
 
 #- Ensure I_MAX_MA <= MOT_I_MAX_MA
-ecmcEpicsEnvSetCalcTernary(I_MAX_MA,"${I_MAX_MA}>${MOT_I_MAX_MA}",${MOT_I_MAX_MA},${I_MAX_MA})
-
-#- Ensure I_MAX_MA <= DRV_I_MAX_MA
-ecmcEpicsEnvSetCalcTernary(I_MAX_MA_VALID,"${I_MAX_MA}>${DRV_I_MAX_MA}",${DRV_I_MAX_MA},${I_MAX_MA})
+ecmcEpicsEnvSetCalcTernary(I_MAX_MA_VALID,"${I_MAX_MA_VALID}>${MOT_I_MAX_MA}",${MOT_I_MAX_MA},${I_MAX_MA})
 
 #-===== Validate voltage ===
-epicsEnvSet(U_NOM_MV, ${U_NOM_MV=${MOT_U_MAX_MV}})
 
-#- Ensure U_NOM_MV <= DRV_U_MAX_MV
-ecmcEpicsEnvSetCalcTernary(U_NOM_MV_VALID,"${U_NOM_MV}>${DRV_U_MAX_MV}",${DRV_U_MAX_MV},${U_NOM_MV})
+#- Reduce the allowed motor voltage if drive cannot support the voltage
+ecmcEpicsEnvSetCalcTernary(MOT_U_MAX_MV,"${MOT_U_MAX_MV}>${DRV_U_MAX_MV}",${DRV_U_MAX_MV},${MOT_U_MAX_MV})
+
+epicsEnvSet(U_NOM_MV_VALID, ${U_NOM_MV=${MOT_U_MAX_MV}})
+
+#- Die if to high current
+ecmcEpicsEnvSetCalcTernary(DIE,"${U_NOM_MV_VALID}>${MOT_U_MAX_MV}","", "#-")
+${DIE}ecmcExit Error: Voltage setting too high for motor
+
+epicsEnvUnset(DIE)
