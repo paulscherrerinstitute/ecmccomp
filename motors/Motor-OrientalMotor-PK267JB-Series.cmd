d7d6d326bed51f5e0ba76613f5a15bbb83e0ed4e
Anders Sandström <anders.sandstroem@psi.ch>
2023-12-18 08:41:57 +0100

    Add motor

--- motors/Motor-OrientalMotor-PK267JB-Series.cmd
+++ motors/Motor-OrientalMotor-PK267JB-Series.cmd
@@ -1,12 +1,12 @@
 #-d /**
-#-d   \brief hardware script for Motor-OrientalMotor-PK267JB-Parallel
+#-d   \brief hardware script for Motor-OrientalMotor-PK267JB-Series
 #-d   \details Parametrization of motor Oriental Motor PK267JB
 #-d   \author Anders Sanddtröm
 #-d   \file
 #-d   \note Max current= 2 A
 #-d   \note Max voltage= 48 V
-#-d   \note Inductance per phase = 3.54mH /  "half" phase => 1.77mH
-#-d   \note Resistance = 2.4 Ohm / "half" phase  => 1.2 Ohm
+#-d   \note Inductance per phase = 3.54mH /  "half" phase  => 7.08mH
+#-d   \note Resistance = 2.4 Ohm / "half" phase => 4.8 Ohm 
 #-d */
 
 #- Component TYPE []
@@ -25,10 +25,10 @@ epicsEnvSet(MOT_U_MAX_MV,48000)
 epicsEnvSet(MOT_U_MV,4800)
 
 #- Phase resistance [mOhm]
-epicsEnvSet(MOT_R_COIL_MOHM,2400)
+epicsEnvSet(MOT_R_COIL_MOHM,4800)
 
 #- Phase inductance [uH]
-epicsEnvSet(MOT_L_COIL_UH,1770)
+epicsEnvSet(MOT_L_COIL_UH,7080)
 
 #- Max Torque [mNm]
 epicsEnvSet(MOT_TRQ_MAX_NMM,1700)
