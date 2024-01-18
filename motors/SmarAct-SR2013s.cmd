eacbe2a9407181714ccfccbbebfda59275770cbe
Anders Sandström <anders.sandstroem@psi.ch>
2024-01-18 13:25:41 +0100

    Add info on smaract homing

--- motors/SmarAct-SR2013s.cmd
+++ motors/SmarAct-SR2013s.cmd
@@ -19,3 +19,29 @@ epicsEnvSet(MOT_HOME_VELO,1000)
 
 #- Homing method id []
 epicsEnvSet(MOT_HOME_METHOD_ID,-128)
+
+#- Supported homing
+#- sandst_a@c6025a:~$ ethercat upload --type int8 -m1 -p0 0x70e3 1 
+#- 0xffffff80 -128
+#- sandst_a@c6025a:~$ ethercat upload --type int8 -m1 -p0 0x70e3 2
+#- 0xffffff81 -127
+#- sandst_a@c6025a:~$ ethercat upload --type int8 -m1 -p0 0x70e3 3
+#- 0xffffff82 -126
+#- sandst_a@c6025a:~$ ethercat upload --type int8 -m1 -p0 0x70e3 4
+#- 0xffffff83 -125
+#- sandst_a@c6025a:~$ ethercat upload --type int8 -m1 -p0 0x70e3 5
+#- 0xffffff88 -120
+#- sandst_a@c6025a:~$ ethercat upload --type int8 -m1 -p0 0x70e3 6
+#- 0xffffff89 -119
+#- sandst_a@c6025a:~$ ethercat upload --type int8 -m1 -p0 0x70e3 7
+#- 0xffffff8a -118
+#- sandst_a@c6025a:~$ ethercat upload --type int8 -m1 -p0 0x70e3 8
+#- 0xffffff8b -117
+#- sandst_a@c6025a:~$ ethercat upload --type int8 -m1 -p0 0x70e3 9
+#- 0x01 1
+#- sandst_a@c6025a:~$ ethercat upload --type int8 -m1 -p0 0x70e3 10
+#- 0x02 2
+#- sandst_a@c6025a:~$ ethercat upload --type int8 -m1 -p0 0x70e3 11
+#- 0x25 37
+#- for some reason 0x60e3 12 does not exists
+#-
