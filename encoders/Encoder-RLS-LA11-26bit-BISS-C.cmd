649ac098f9adc5c40d3aadf20dfd39699817c1ac
Felix Maier <felix.maier@psi.ch>
2024-03-18 08:40:09 +0100

    Add support for RLS-LA11-26bit encoder (BiSS-C)

---
+++ encoders/Encoder-RLS-LA11-26bit-BISS-C.cmd
@@ -0,0 +1,42 @@
+#-d /**
+#-d   \brief Script for Encoder-RLS-LA11-26bit
+#-d   \details RLS Biss-C 26-bit encoder, sends a 26-bit position, while lower resolution models fix the LSB to 0.
+#-d   \author Felix Maier 
+#-d   \file
+#-d   \note Supply:     5 V
+#-d */
+
+#- Component TYPE []
+epicsEnvSet(COMP_TYPE,BISS_C_ENC)
+
+#- 0x80p8:02: Disable Status Bits (default 0)
+epicsEnvSet(ENC_DIS_STAT_BITS,0)
+
+#- 0x80p8:03: CRC Invert (default 1 ?)
+epicsEnvSet(ENC_CRC_INV,1)
+
+#- 0x80p8:11: CRC Polynom (default 67)
+epicsEnvSet(ENC_CRC_POLY,67)
+
+#- Supply voltage [mV]
+epicsEnvSet(ENC_U_SUP_VLT_MV,5000)
+
+#- 0x80p8:13: Clock Frequency [kHz]
+epicsEnvSet(ENC_CLK_FRQ_KHZ,2500)
+
+#- 0x80p8:14: Coding
+#- 0: Dual code active (default)
+#- 1: Gray code active
+epicsEnvSet(ENC_CODING,0)
+
+#- 0x80p8:15: Multiturn [Bit] Number of multiturn bits
+#- default 12
+epicsEnvSet(ENC_MT_BITS,0)
+
+#- 0x80p8:16: Singleturn [Bit] Number of singleturn bits
+#- default 13
+epicsEnvSet(ENC_ST_BITS,26)
+
+#- 0x80p8:17: Offset LSB Bit [Bit] Number of “right aligned” Offset bits
+#- default 0
+epicsEnvSet(ENC_LSB_OFF_BITS,0)
