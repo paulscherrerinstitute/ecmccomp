3ae3f0f67ade03fcfaacf9a67d7e5a0a1209a65c
Anders Sandström <anders.sandstroem@psi.ch>
2024-10-23 10:09:21 +0200

    Use correct macro for incremental encoder single turn

--- encoder_slaves/hw_sdo_scripts/Festo-CMMT-ST_SDOS_INC_ENC.cmd
+++ encoder_slaves/hw_sdo_scripts/Festo-CMMT-ST_SDOS_INC_ENC.cmd
@@ -21,7 +21,7 @@ ecmcConfigOrDie "Cfg.EcAddSdoDT(${COMP_S_ID},0x2130,0x7D,4,S32)"
 
 #- Singleturn resolution
 #- 0x2138:01: Single turn resolution 
-ecmcConfigOrDie "Cfg.EcAddSdoDT(${COMP_S_ID},0x2138,0x1,${ST_ENC_RES=${ENC_ST_ENC_RES=500}},U32)"
+ecmcConfigOrDie "Cfg.EcAddSdoDT(${COMP_S_ID},0x2138,0x1,${ST_ENC_RES=${ENC_ST_RES=500}},U32)"
 epicsEnvUnset(ST_ENC_RES)
 
 # Encoder homed P0.2337.0.0
--- scripts/support/cleanupINC_ENC.cmd
+++ scripts/support/cleanupINC_ENC.cmd
@@ -2,4 +2,4 @@
 # cleanupINC_ENC.cmd
 epicsEnvUnset(INV_DIR)
 epicsEnvUnset(ST_ENC_RES)
-epicsEnvUnset(ENC_ST_ENC_RES)
+epicsEnvUnset(ENC_ST_RES)
