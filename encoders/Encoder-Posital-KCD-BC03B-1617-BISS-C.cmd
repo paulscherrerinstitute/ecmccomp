212065664af9ed3218ea1bb05f709385663a3c22
Anders Sandström <anders.sandstroem@psi.ch>
2024-11-19 11:50:03 +0100

    Add Posital kit encoder

---
+++ encoders/Encoder-Posital-KCD-BC03B-1617-BISS-C.cmd
@@ -0,0 +1,41 @@
+#-d /**
+#-d   \brief Script for Encoder-Posital-KCD-BC03B 1617
+#-d   \details  Posital-KCD-BC03B 1617
+#-d   \file
+#-d   \note Supply:     5 V
+#-d */
+
+#- Component TYPE []
+epicsEnvSet(COMP_TYPE,BISS_C_ENC)
+
+#- 0x80p8:02: Disable Status Bits (default 0)
+epicsEnvSet(ENC_DIS_STAT_BITS,0)
+
+#- 0x80p8:03: CRC Invert (default 1 ?)
+epicsEnvSet(ENC_CRC_INV,1)
+
+#- 0x80p8:11: CRC Polynom (default 67)
+epicsEnvSet(ENC_CRC_POLY,67)
+
+#- Supply voltage [mV]
+epicsEnvSet(ENC_U_SUP_VLT_MV,5000)
+
+#- 0x80p8:13: Clock Frequency [kHz]
+epicsEnvSet(ENC_CLK_FRQ_KHZ,1000)
+
+#- 0x80p8:14: Coding
+#- 0: Dual code active (default)
+#- 1: Gray code active
+epicsEnvSet(ENC_CODING,0)
+
+#- 0x80p8:15: Multiturn [Bit] Number of multiturn bits
+#- default 12
+epicsEnvSet(ENC_MT_BITS,16)
+
+#- 0x80p8:16: Singleturn [Bit] Number of singleturn bits
+#- default 13
+epicsEnvSet(ENC_ST_BITS,17)
+
+#- 0x80p8:17: Offset LSB Bit [Bit] Number of “right aligned” Offset bits
+#- default 0
+epicsEnvSet(ENC_LSB_OFF_BITS,0)
